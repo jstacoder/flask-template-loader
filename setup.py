7e46d283fcbaac07394e94a8d9f2893088c6f62e
kyle roux <kyle@level2designs.com>
2015-09-23 11:36:07 -0700

    upping version

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     packages=['flask_template_loader'],
     install_requires=['flask'],
     url='git@github.com:jstacoder/flask-template-loader.git',
-    version='0.0.2',
+    version='0.0.3',
 )
